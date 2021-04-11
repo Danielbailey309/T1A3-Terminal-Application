27e1b92d4d78383be3e6ae824cca807b89d0fc13
daniel <danielbailey309@gmail.com>
2021-04-11 17:46:10 +1000

    colorize working

--- src/main.rb
+++ src/main.rb
@@ -1,5 +1,8 @@
 require_relative 'rock'
 
+require 'colorize'
+require 'tty-prompt'
+
 # Creating variables
 menu = 
 menu_options = ["list", "identify", "create", "save", "collection", "quit"]
@@ -20,34 +23,36 @@ Rock.new("Shale", "black", "rock", "sedimentary", "fine", "none", "3")
 
 # Main program. Essentially asks the user what they want to do, and then runs the method associated with that command
 begin
-    while menu != "quit"
-        puts "Please enter a command"
-        puts "Type 'list' to list all the rocks currently in the database"
-        puts "Type 'create' to add a new rock to the database"
-        puts "Type 'identify' to identify a new rock that you've found"
-        puts "Type 'save' to save a rock from the database to your collection"
-        puts "Type 'collection' to view your collection"
-        puts "Type 'quit' to exit the application"
-        menu = gets.chomp.downcase
-        raise "That's not a valid selection" unless menu_options.include?(menu)
-        if menu == "list"
+    while menu != 6
+        question = "What would you like to do?".colorize :blue
+        choices = {
+            "List all rocks currently in the database" => 1,
+            "Add a new rock to the database" => 2,
+            "Identify a new rock that you've found" => 3
+            "Save a rock from the database to your collection" => 4,
+            "View your collection" => 5,
+            "Exit the application" => 6
+        }
+        menu = @@prompt.select(question, choices)
+        raise "That's not a valid selection" unless choices.include?(menu)
+        if menu == 1
             puts "The following rocks are in the database:"
             Rock.all_rocks.each { |rock| puts rock.name }
         end
-        if menu == "create"
+        if menu == 2
             Rock.createrock
         end
-        if menu == "identify"
+        if menu == 3
             Rock.identifyrock
         end
-        if menu == "save"
+        if menu == 4
             Rock.saverock
         end
-        if menu == "collection"
+        if menu == 5
             puts "You have the following rocks in your collection:"
             puts Rock.rock_collection
         end
     end
 rescue Exception => e
-    puts "An error occurred: #{e}"
+    puts "An error occurred: #{e}".colorize :red
 end
--- src/rock.rb
+++ src/rock.rb
@@ -1,7 +1,10 @@
+require 'tty-prompt'
+
 class Rock
     attr_reader :name, :colour, :type, :formation, :grain_size, :streak, :hardness
     @@rock_arr = []
     @@rock_collection_arr = []
+    @@prompt = TTY::Prompt.new
     def initialize(name, colour, type, formation, grain_size, streak, hardness)
         @name = name
         @colour = colour
@@ -24,7 +27,7 @@ class Rock
     # When the user types "identify" in the menu, this method will ask the user a series of questions to find out which rock it is
     def self.identifyrock
         identifyarr = []
-        puts "What colour is the rock?"
+        puts "What colour is the rock?".colorize :blue
         colour_input = gets.chomp
         @@rock_arr.each do |rock|
             if colour_input == rock.colour
@@ -32,35 +35,38 @@ class Rock
             end
         end
 
-        puts "Is it a rock or a mineral?"
+        puts "Is it a rock or a mineral?".colorize :blue
         type_input = gets.chomp
         identifyarr.filter! {|rock| rock.type == type_input }
 
-        puts "If it is a rock, how is it formed? (rocks can be sedimentary, igneous or metamorphic)"
-        formation_input = gets.chomp
+        question = "If it is a rock, how is it formed?".colorize :blue
+        choices = ["sedimentary", "igneous", "metamorhic"]
+        formation_input = @@prompt.select(question, choices)
         identifyarr.filter! {|rock| rock.formation == formation_input }
 
-        puts "How large is the grain of the rock? (the grain can be coarse, medium or fine)"
-        grain_size_input = gets.chomp
+        question = "How large is the grain of the rock?".colorize :blue
+        choices = ["coarse", "medium", "fine"]
+        grain_size_input = @@prompt.select(question, choices)
         identifyarr.filter! {|rock| rock.grain_size == grain_size_input }
 
-        puts "What colour streak does the mineral leave whem scraped across paper?"
+        puts "What colour streak does the mineral leave whem scraped across paper?".colorize :blue
         streak_input = gets.chomp
         identifyarr.filter! {|rock| rock.streak == streak_input }
 
-        puts "What is the hardness of the rock or mineral, on a scale of 1 to 10?"
+        puts "What is the hardness of the rock or mineral, on a scale of 1 to 10?".colorize :blue
         hardness_input = gets.chomp
         raise "That is not a valid hardness (must be between 1 and 10)" if hardness_input.to_i < 1 or hardness_input.to_i > 10
         identifyarr.filter! {|rock| rock.hardness == hardness_input }
 
-        puts "Your rock is one of the following:"
+        raise "That rock is not in the database" if identifyarr == []
+        puts "Your rock is one of the following:".colorize :green
         identifyarr.each { |rock| puts rock.name }
     end
 
     # When the user types "save" in the menu, this method will save the rock to the user's collection
     def self.saverock
         rock_collection_temp_arr = []
-        puts "What is the name of the rock you want to add to your collection?"
+        puts "What is the name of the rock you want to add to your collection?".colorize :blue
         save_input = gets.chomp.capitalize
         @@rock_arr.each do |rock|
             if save_input == rock.name
@@ -75,25 +81,25 @@ class Rock
     # then add a rock to the database with the specifications entered
     def self.createrock
 
-        puts "What is the name of the rock?"
+        puts "What is the name of the rock?".colorize :blue
         name = gets.chomp.capitalize
         
-        puts "What colour is the rock?"
+        puts "What colour is the rock?".colorize :blue
         colour = gets.chomp
         
-        puts "Is it a rock or a mineral?"
+        puts "Is it a rock or a mineral?".colorize :blue
         type = gets.chomp
         
-        puts "If it is a rock, how is it formed? (rocks can be sedimentary, igneous or metamorphic)"
+        puts "If it is a rock, how is it formed? (rocks can be sedimentary, igneous or metamorphic)".colorize :blue
         formation = gets.chomp
         
-        puts "How large is the grain of the rock? (the grain can be coarse, medium or fine)"
+        puts "How large is the grain of the rock? (the grain can be coarse, medium or fine)".colorize :blue
         grain_size = gets.chomp
         
-        puts "What colour streak does the mineral leave whem scraped across paper?"
+        puts "What colour streak does the mineral leave whem scraped across paper?".colorize :blue
         streak = gets.chomp
         
-        puts "What is the hardness of the rock or mineral, on a scale of 1 to 10?"
+        puts "What is the hardness of the rock or mineral, on a scale of 1 to 10?".colorize :blue
         hardness = gets.chomp
         raise "That is not a valid hardness (must be between 1 and 10)" if hardness.to_i < 1 or hardness.to_i > 10
         
